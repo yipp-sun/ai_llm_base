ad02c12528f689c69e9e682b42498cc0c803a258
yipp <song_ontatu@163.com>
2025-02-24 19:10:56 +0800

    upd

--- day5-demo/example/prompt_templates.py
+++ day5-demo/example/prompt_templates.py
@@ -2,12 +2,14 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
-model = ChatOpenAI(model="gpt-4")
+model = ChatOpenAI(model="gpt-4o-mini")
 system_template = "Translate the following into {language}:"
 prompt_template = ChatPromptTemplate.from_messages(
     [("system", system_template), ("user", "{text}")]
 )
 result = prompt_template.invoke({"language": "Chinese", "text": "hi"})
+print(result)
+# messages=[SystemMessage(content='Translate the following into Chinese:', additional_kwargs={}, response_metadata={}), HumanMessage(content='hi', additional_kwargs={}, response_metadata={})]
 print(result.to_messages())
 # [SystemMessage(content='Translate the following into Chinese:'), HumanMessage(content='hi')]
 
